2def7b5e1f54db818a1ea260c9c65e4792f08bde
tabchen <2808581543@qq.com>
2022-06-15 15:25:59 +0800

    update

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,7 +1,7 @@
 from paper_finder.api import download_from_df
 import pandas as pd
 
-csv_path = "kt_result.csv"
+csv_path = "result.csv"
 df = pd.read_csv(csv_path)
 df = download_from_df(df,save_dir='pdfs')
 df.to_csv(csv_path.replace('.csv','_download_result.csv'),index=False)
